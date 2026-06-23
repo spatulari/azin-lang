3ae8f6ef188548ed8526f0d3822defdf3d67ea66
yydev-official <sihamelmachhouri@gmail.com>
2026-06-23 17:45:25 +0200

    Add helpers and version sources, refactor main
    Formatted header files for consistent indentation.

--- cli/include/azin/version.hpp
+++ cli/include/azin/version.hpp
@@ -1,6 +1,6 @@
 #ifndef AZIN_CLI_VERSION_H
-#define AZIN_CLI_VERSION_H
+    #define AZIN_CLI_VERSION_H
 
-int versionCommand(int argc, char* argv[]);
+    int versionCommand(int argc, char* argv[]);
 
 #endif
