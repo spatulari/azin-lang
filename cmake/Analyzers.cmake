dde021d3e175ab65ec8a64af98b44d05d036fef4
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-06-27 12:44:20 +0300

    Add azin_enable_analyzers

--- cmake/Analyzers.cmake
+++ cmake/Analyzers.cmake
@@ -10,40 +10,36 @@ find_program(AZIN_CLANG_TIDY_EXE NAMES clang-tidy)
 find_program(AZIN_CPPCHECK_EXE NAMES cppcheck)
 find_program(AZIN_IWYU_EXE NAMES include-what-you-use)
 
-set(AZIN_HAS_CLANG_TIDY OFF CACHE INTERNAL "")
-set(AZIN_HAS_CPPCHECK OFF CACHE INTERNAL "")
-set(AZIN_HAS_IWYU OFF CACHE INTERNAL "")
+set(AZIN_HAS_CLANG_TIDY OFF)
+set(AZIN_HAS_CPPCHECK OFF)
+set(AZIN_HAS_IWYU OFF)
 
-# MSVC and clang-tidy don't work well together from what I've tested; use clang-cl instead.
 if (AZIN_ENABLE_ANALYZERS
         AND AZIN_ENABLE_CLANG_TIDY
         AND AZIN_CLANG_TIDY_EXE
         AND NOT MSVC)
 
-    set(_clang_tidy_args
+    set(AZIN_CLANG_TIDY_COMMAND
             ${AZIN_CLANG_TIDY_EXE}
             --extra-arg=-Wno-unknown-warning-option
             --extra-arg=-Wno-error=unknown-warning-option
+            --exclude-header-filter=^${CMAKE_BINARY_DIR}
     )
 
     if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        list(APPEND _clang_tidy_args
+        list(APPEND AZIN_CLANG_TIDY_COMMAND
                 --extra-arg-before=--driver-mode=g++)
     elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-        list(APPEND _clang_tidy_args
+        list(APPEND AZIN_CLANG_TIDY_COMMAND
                 --extra-arg-before=--driver-mode=clang++)
     endif ()
 
     if (AZIN_ANALYZERS_STRICT)
-        list(APPEND _clang_tidy_args
+        list(APPEND AZIN_CLANG_TIDY_COMMAND
                 --warnings-as-errors=*)
     endif ()
 
-    set(CMAKE_CXX_CLANG_TIDY
-            "${_clang_tidy_args}"
-            CACHE STRING "" FORCE)
-
-    set(AZIN_HAS_CLANG_TIDY ON CACHE INTERNAL "" FORCE)
+    set(AZIN_HAS_CLANG_TIDY ON)
 
     message(STATUS "Analyzer: clang-tidy enabled")
 endif ()
@@ -52,7 +48,7 @@ if (AZIN_ENABLE_ANALYZERS
         AND AZIN_ENABLE_CPPCHECK
         AND AZIN_CPPCHECK_EXE)
 
-    set(_cppcheck_args
+    set(AZIN_CPPCHECK_COMMAND
             ${AZIN_CPPCHECK_EXE}
             --enable=warning,performance,portability
             --inline-suppr
@@ -60,15 +56,11 @@ if (AZIN_ENABLE_ANALYZERS
     )
 
     if (AZIN_ANALYZERS_STRICT)
-        list(APPEND _cppcheck_args
+        list(APPEND AZIN_CPPCHECK_COMMAND
                 --error-exitcode=1)
     endif ()
 
-    set(CMAKE_CXX_CPPCHECK
-            "${_cppcheck_args}"
-            CACHE STRING "" FORCE)
-
-    set(AZIN_HAS_CPPCHECK ON CACHE INTERNAL "" FORCE)
+    set(AZIN_HAS_CPPCHECK ON)
 
     message(STATUS "Analyzer: cppcheck enabled")
 endif ()
@@ -77,10 +69,27 @@ if (AZIN_ENABLE_ANALYZERS
         AND AZIN_ENABLE_IWYU
         AND AZIN_IWYU_EXE)
 
-    set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
+    set(AZIN_IWYU_COMMAND
             ${AZIN_IWYU_EXE})
 
-    set(AZIN_HAS_IWYU ON CACHE INTERNAL "" FORCE)
+    set(AZIN_HAS_IWYU ON)
 
     message(STATUS "Analyzer: include-what-you-use enabled")
 endif ()
+
+function(azin_enable_analyzers target)
+    if (AZIN_HAS_CLANG_TIDY)
+        set_property(TARGET ${target} PROPERTY
+                CXX_CLANG_TIDY "${AZIN_CLANG_TIDY_COMMAND}")
+    endif ()
+
+    if (AZIN_HAS_CPPCHECK)
+        set_property(TARGET ${target} PROPERTY
+                CXX_CPPCHECK "${AZIN_CPPCHECK_COMMAND}")
+    endif ()
+
+    if (AZIN_HAS_IWYU)
+        set_property(TARGET ${target} PROPERTY
+                CXX_INCLUDE_WHAT_YOU_USE "${AZIN_IWYU_COMMAND}")
+    endif ()
+endfunction()
