9b5c836a1dedc3f9711881d252eb579c3dad478f
Alecu Ștefan-Iulian <165364995+pascalecu@users.noreply.github.com>
2026-07-11 12:06:47 +0300

    Store Position instead of Line, Column and Offset and remove format helpers

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,46 +1,20 @@
 package token
 
-import "fmt" // provides: Sprintf
-
-// Token represents a token parsed from the source code.
 type Token struct {
-	Kind   Kind
-	Offset uint32
-	Length uint32
-	Line   uint32
-	Column uint32
+	Kind     Kind
+	Position Position
+	Length   uint32
 }
 
-// String returns a string representation of the token.
-func (t Token) String() string {
-	return fmt.Sprintf(
-		"%-18s %4d:%4d [%d:%d]",
-		t.Kind,
-		t.Line,
-		t.Column,
-		t.Offset,
-		t.Length,
-	)
-}
-
-// Format returns a formatted string representation of the token.
-func (t Token) Format(source []byte) string {
-	s := fmt.Sprintf(
-		"%-18s %4d:%4d [%d:%d]",
-		t.Kind,
-		t.Line,
-		t.Column,
-		t.Offset,
-		t.Length,
-	)
-
-	// If the token is a literal, append its value to the string.
-	switch t.Kind {
-	case Identifier, IntegerLiteral, FloatLiteral, StringLiteral, CharacterLiteral:
-		if end := t.Offset + t.Length; end <= uint32(len(source)) {
-			s += fmt.Sprintf(" %q", source[t.Offset:end])
-		}
+func (k Kind) HasText() bool {
+	switch k {
+	case Identifier,
+		IntegerLiteral,
+		FloatLiteral,
+		StringLiteral,
+		CharacterLiteral:
+		return true
+	default:
+		return false
 	}
-
-	return s
 }
