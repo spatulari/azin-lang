d4fbad49f400a742833e02cc8576046afee37a5e
Alecu Ștefan-Iulian <165364995+pascalecu@users.noreply.github.com>
2026-07-11 09:14:51 +0300

    Shorten main function

--- cmd/compiler/compiler.go
+++ cmd/compiler/compiler.go
@@ -9,83 +9,66 @@ import (
 	"github.com/azin-lang/Azin/internal/fs"
 )
 
-const VERSION = "0.2.3-dev"
+const Version = "0.2.3-dev"
 
 var (
-	debug           = flag.Bool("debug", false, "")
-	printTokens     = flag.Bool("tokens", false, "")
-	output          = flag.String("o", "", "")
-	ignoreExtension = flag.Bool("ignore-extension", false, "")
-	version         = flag.Bool("version", false, "")
+	debug           = flag.Bool("debug", false, "Enable debug output")
+	printTokens     = flag.Bool("tokens", false, "Print lexer tokens")
+	output          = flag.String("o", "", "Output file")
+	ignoreExtension = flag.Bool("ignore-extension", false, "Ignore source file extension")
+	version         = flag.Bool("version", false, "Print compiler version")
 )
 
-// handleHelp prints the help message and exits if the help flag is set
-func handleHelp() {
-	if flag.NArg() != 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [flags] <filename>\n", os.Args[0])
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [flags] <file>\n\n", os.Args[0])
 		flag.PrintDefaults()
-		os.Exit(1)
 	}
 }
 
-// handleDebug prints debug information if the debug flag is set
-func handleDebug() {
-	if *debug {
-		fmt.Println("Debug:", *debug)
-		fmt.Println("Print tokens:", *printTokens)
-		fmt.Println("Output:", *output)
-	}
-}
+func main() {
+	flag.Parse()
 
-// if the version flag is set, printVersion prints the version and exits
-func printVersion() {
-	fmt.Println("Azin compiler version", VERSION)
-	os.Exit(0)
-}
-
-// checkVersionFlag prints the version if the version flag is set
-func checkVersionFlag() {
 	if *version {
-		printVersion()
+		fmt.Printf("Azin compiler %s\n", Version)
+		return
 	}
-}
 
-// loadSource reads the source file from the filesystem
-func loadSource(filename string) []byte {
-	source, err := fs.ReadSourceFile(filename, *ignoreExtension)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	if flag.NArg() != 1 {
+		flag.Usage()
 		os.Exit(1)
 	}
-	return source
-}
 
-// runCompiler compiles the source code and prints any errors
-func runCompiler(source []byte) {
+	if *debug {
+		printDebug()
+	}
+
+	source := mustReadSource(flag.Arg(0))
+
 	if err := compiler.Compile(source, *printTokens); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		fatal(err)
 	}
-}
 
-// printCompileStats prints the compiled size in KiB if debug mode is enabled
-func printCompileStats(source []byte) {
 	if *debug {
 		fmt.Printf("Compiled %.2f KiB\n", float64(len(source))/1024)
 	}
 }
 
-// main is the entry point of the compiler
-func main() {
-	flag.Parse()
+func printDebug() {
+	fmt.Printf("Debug: %t\n", *debug)
+	fmt.Printf("Print tokens: %t\n", *printTokens)
+	fmt.Printf("Output: %q\n", *output)
+}
 
-	checkVersionFlag()
-	handleHelp()
+func mustReadSource(filename string) []byte {
+	source, err := fs.ReadSourceFile(filename, *ignoreExtension)
+	if err != nil {
+		fatal(err)
+	}
+	return source
+}
 
-	filename := flag.Arg(0)
-	handleDebug()
-
-	source := loadSource(filename)
-	runCompiler(source)
-	printCompileStats(source)
+func fatal(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
 }
