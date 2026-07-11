41953a5b0b076d3fef0d606a8be3e7c875aafe3f
yydev-official <sihamelmachhouri@gmail.com>
2026-07-11 14:07:04 +0200

    Update main.go
    Co-authored-by: Alecu Ștefan-Iulian <165364995+pascalecu@users.noreply.github.com>

--- cmd/compiler/main.go
+++ cmd/compiler/main.go
@@ -24,7 +24,6 @@ var (
 	version         = flag.Bool("version", false, "Print compiler version")
 )
 
-// init sets the usage function for the flag package.
 func init() {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [flags] <file>\n\n", os.Args[0])
@@ -32,7 +31,6 @@ func init() {
 	}
 }
 
-// main is the entry point of the compiler.
 func main() {
 	flag.Parse()
 
@@ -75,15 +73,12 @@ func main() {
 	}
 }
 
-// printDebug prints the debug information.
 func printDebug() {
 	fmt.Printf("Debug: %t\n", *debug)
 	fmt.Printf("Print tokens: %t\n", *printTokens)
 	fmt.Printf("Output: %q\n", *output)
 }
 
-// mustReadSource reads the source file and returns its contents as a byte slice.
-// It exits the program if the file cannot be read.
 func mustReadSource(filename string) []byte {
 	source, err := fs.ReadSourceFile(filename, *ignoreExtension)
 	if err != nil {
@@ -92,13 +87,11 @@ func mustReadSource(filename string) []byte {
 	return source
 }
 
-// fatal prints the error and exits the program.
 func fatal(err error) {
 	fmt.Fprintln(os.Stderr, err)
 	os.Exit(1)
 }
 
-// formatToken formats a token as a string for debugging purposes.
 func formatToken(f *source.File, tok token.Token) string {
 	line, column := f.LineColumn(tok.Position.Offset)
 
