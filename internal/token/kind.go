4ae45ea90b24b040cf39496082d53b30ba214b8f
Alecu Ștefan-Iulian <165364995+pascalecu@users.noreply.github.com>
2026-07-13 20:58:04 +0300

    Add bool and null token kinds

--- internal/token/kind.go
+++ internal/token/kind.go
@@ -1,6 +1,5 @@
 package token
 
-//// go:generate go run golang.org/x/tools/cmd/stringer -type=Kind -linecomment
 //go:generate go tool stringer -type=Kind -linecomment
 
 // Kind is the set of lexical token types.
@@ -23,6 +22,8 @@ const (
 	KwEnd     // kw_end
 	KwChar    // kw_char
 	KwInt     // kw_int
+	KwBool    // kw_bool
+	KwNull    // kw_null
 	KwUnit    // kw_unit
 	KwString  // kw_string
 	KwFloat   // kw_float
